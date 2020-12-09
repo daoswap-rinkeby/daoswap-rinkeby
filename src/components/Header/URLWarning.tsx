39ec62f7d625e1cb5bae810bfe1589c8aeddacea
daoswap001 <daoswap@daoswap.global>
2020-12-09 17:11:27 +0800

    first commit

--- src/components/Header/URLWarning.tsx
+++ src/components/Header/URLWarning.tsx
@@ -31,15 +31,15 @@ export default function URLWarning() {
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={12} /> Make sure the URL is
-        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>app.daoswap.global</code>
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>rinkeby-app.daoswap.global</code>
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
     </PhishAlert>
-  ) : window.location.hostname === 'app.daoswap.global' ? (
+  ) : window.location.hostname === 'rinkeby-app.daoswap.global' ? (
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={12} /> Always make sure the URL is
-        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>app.daoswap.global</code> - bookmark it
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>rinkeby-app.daoswap.global</code> - bookmark it
         to be safe.
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
